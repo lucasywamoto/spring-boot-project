b2eed137c23569d6112d5f0441a1ef82d5ec5ddd
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2024-03-25 16:27:15 -0400

    Payment, one-to-one association

--- src/main/java/com/lucasywamoto/demo/config/TestConfig.java
+++ src/main/java/com/lucasywamoto/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucasywamoto.demo.entities.Category;
 import com.lucasywamoto.demo.entities.Order;
 import com.lucasywamoto.demo.entities.OrderItem;
+import com.lucasywamoto.demo.entities.Payment;
 import com.lucasywamoto.demo.entities.Product;
 import com.lucasywamoto.demo.entities.User;
 import com.lucasywamoto.demo.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
